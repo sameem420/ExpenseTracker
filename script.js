96210f2f1eacf625535a57ec39cffc5971b7009a
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-09-16 09:50:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var totalBalance = document.querySelector("#totalbalance ").innerHTML;
+var totalBalance = document.querySelector("#totalbalance").innerHTML;
 console.log(totalBalance);
 
-var incomeBalance = document.querySelector("#incomebalance ").innerHTML;
+var incomeBalance = document.querySelector("#incomebalance").innerHTML;
 console.log(incomeBalance);
 
-var expenseBalance = document.querySelector("#expensebalance ").innerHTML;
+var expenseBalance = document.querySelector("#expensebalance").innerHTML;
 console.log(expenseBalance);
+
+var amount = document.querySelector("#amount").innerHTML;
+console.log(amount);
