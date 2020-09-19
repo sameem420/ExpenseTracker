f372684131f28e6ef78db53529c35d76c374228c
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-09-19 12:25:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var totalBalance = document.querySelector("#totalbalance");
 var incomeBalance = document.querySelector("#incomebalance");
 var expenseBalance = document.querySelector("#expensebalance");
-
+var transactionRecord = document.querySelector("#transactionsrecord");
 
 var btnAddTransaction = document.getElementById("btnAddtransaction");
 
 function getAmount(){
+    var expesneType = document.getElementById("expensetype").value;
     var amount = document.getElementById("amount").value;
+    var record = document.createElement("div");
+    var incomeexpense = document.createTextNode(expesneType + " " + amount);
+    record.appendChild(incomeexpense);
+    transactionRecord.appendChild(record);
+    
     if(amount == "")
     {
         alert("Please enter an amount first");
