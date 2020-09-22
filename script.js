fbfb0c651249796aec1f7e827518adefd079da39
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-09-22 23:47:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var totalBalance = document.querySelector("#totalbalance");
+
 var incomeBalance = document.querySelector("#incomebalance");
+
 var expenseBalance = document.querySelector("#expensebalance");
+
 var transactionRecord = document.querySelector("#transactionsrecord");
 
 var btnAddTransaction = document.getElementById("btnAddtransaction");
+
 
 function getAmount(){
     var expesneType = document.getElementById("expensetype").value;
